24ec1bd79f55eb999d88ee4538ec1d2c9875ba46
Juan Motta <jrmotta119@gmail.com>
2025-07-06 23:16:51 -0400

    Fix4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,26 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { tempo } from "tempo-devtools/dist/vite";
-
-const conditionalPlugins: [string, Record<string, any>][] = [];
-
-// @ts-ignore
-if (process.env.TEMPO === "true") {
-  conditionalPlugins.push(["tempo-devtools/swc", {}]);
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  optimizeDeps: {
-    entries: ["src/main.tsx", "src/tempobook/**/*"],
-  },
   plugins: [
-    react({
-      plugins: conditionalPlugins,
-    }),
-    tempo(),
+    react(),
   ],
   resolve: {
     preserveSymlinks: true,
@@ -31,6 +17,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      external: ['tempo-routes', 'tempo-devtools'],
+    },
   },
   server: {
     // @ts-ignore
