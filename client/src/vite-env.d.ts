1fbf72503c67c935120aff2817b31d820a736adb
Juan Motta <jrmotta119@gmail.com>
2025-07-06 23:01:21 -0400

    Fix2

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
-  readonly TEMPO: string
+  readonly VITE_TEMPO: string
   // more env variables...
 }
 
